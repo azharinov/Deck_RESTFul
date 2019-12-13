a73ee154c7cf611f4540cd7ce1e6574418358ab0
Aleksandr Zharinov <zharinov.alexander@gmail.com>
2019-12-13 11:00:01 +0500

    Исправил ошибку в алгоритме тасования

--- Deck.RESTFul/Deck.Logic/Entities/FastShuffler.cs
+++ Deck.RESTFul/Deck.Logic/Entities/FastShuffler.cs
@@ -21,7 +21,7 @@ namespace Deck.Logic.Entities
             return await Task.Run(() =>
             {
                 var rnd = new Random();
-                var newItems = new LinkedList<ICard>();
+                var newItems = new List<ICard>(items.Count);
 
                 int rndIndex = 0;
                 int count = items.Count;
@@ -29,11 +29,11 @@ namespace Deck.Logic.Entities
                 for (int index = 0; index < count; index++)
                 {
                     rndIndex = rnd.Next(items.Count);
-                    newItems.AddLast(items[rndIndex]);
+                    newItems.Add(items[rndIndex]);
                     items.RemoveAt(rndIndex);
                 }
 
-                return newItems.ToList();
+                return newItems;
             });
         }
     }
